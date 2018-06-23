37ab516767b80a531c8f75b7eaf8c22b73e6ce58
sureshkareti <sureshkareti117@gmail.com>
2018-06-23 15:02:12 +0530

    updates

--- ravi/script.js
+++ ravi/script.js
@@ -1,16 +1,40 @@
 $(document).ready(function(){
+	var isMobile = {
+	    Android: function() {
+	        return navigator.userAgent.match(/Android/i);
+	    },
+	    BlackBerry: function() {
+	        return navigator.userAgent.match(/BlackBerry/i);
+	    },
+	    iOS: function() {
+	        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+	    },
+	    Opera: function() {
+	        return navigator.userAgent.match(/Opera Mini/i);
+	    },
+	    Windows: function() {
+	        return navigator.userAgent.match(/IEMobile/i);
+	    },
+	    any: function() {
+	        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+	    }
+	};
+
 	$(".variable").slick({
 	  dots: true,
 	  infinite: true,
 	  autoplay:true
 	});
-  $('.navbar-toggler').click(function(){
-	   $(this).toggleClass("change");
-	  $(this).next('.navbar-collapse').slideToggle();
-  });
-  $('.dropdown').click(function(e){
-	  e.preventDefault();
-	  $(this).toggleClass('open');
-	  $(this).children('.dropdown-list').slideToggle();
-  }) 
+	if( isMobile.any() ) {
+		$('.navbar-toggler').click(function(){
+		   	$(this).toggleClass("change");
+		  	$(this).next('.navbar-collapse').slideToggle();
+	  	});
+	  	$('.dropdown').click(function(e){
+		  e.preventDefault();
+		  $(this).toggleClass('open');
+		  $(this).children('.dropdown-list').slideToggle();
+	  	}) 
+	}
+  
 })
